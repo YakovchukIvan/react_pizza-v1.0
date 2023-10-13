5161c3a05a853f612d14f1bf565ea085dff3a38e
YakovchukIvan <litvinovjobs@gmail.com>
2023-10-13 20:53:21 +0300

    add components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+// Функціональний компонент
+
+// const Button = () => {
+//   return (
+//     <>
+//       <button></button>
+//     </>
+//   );
+// };
+
+// export default Button;
+
+// Класовий компонент
+
+class Button extends React.Component {
+  render() {
+    return <button>Додати</button>;
+  }
+}
+
+export default Button;
